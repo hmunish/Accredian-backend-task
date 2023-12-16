ff6b906a91577401b145fdb1ba92fc5de1713c1f
Munish <hmunish5@gmail.com>
2023-12-16 14:28:52 +0200

    Add user model for sequelize

---
+++ models/users.js
@@ -0,0 +1,32 @@
+const Sequelize = require('sequelize');
+const database = require('../utilities/database');
+
+const User = database.define('users', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  username: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+},
+{
+  indexes: [
+    { fields: ['email'] },
+    { fields: ['username'] },
+  ],
+},
+);
+
+module.exports = User;
